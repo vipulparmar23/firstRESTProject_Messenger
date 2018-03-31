4c90a814aa039c7a07d0fc4154c15876598ccabb
vipul parmar <vipulparmar154@gmail.com>
2018-03-30 20:02:25 -0700

    adding all project files

---
+++ src/main/java/com/vipul/rest/messenger/model/Message.java
@@ -0,0 +1,46 @@
+package com.vipul.rest.messenger.model;
+
+public class Message {
+
+	private long id;
+	private String author;
+	private String message;
+	private String createdOn;
+	
+	
+	public Message() {
+		
+	}
+	
+	public Message(long id, String message, String author) {
+		this.createdOn = createdOn;
+		this.id = id;
+		this.author = author;
+		this.message = message;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+}
---
+++ src/main/java/com/vipul/rest/messenger/service/MessageService.java
@@ -0,0 +1,7 @@
+package com.vipul.rest.messenger.service;
+
+public class MessageService {
+
+	
+	
+}
