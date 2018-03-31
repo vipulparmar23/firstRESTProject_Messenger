10c094fdad8b78d5606169c2995a9c42a0b2b23b
vipul parmar <vipulparmar154@gmail.com>
2018-03-30 20:23:55 -0700

    pushing all

--- src/main/java/com/vipul/rest/messenger/service/MessageService.java
+++ src/main/java/com/vipul/rest/messenger/service/MessageService.java
@@ -1,7 +1,13 @@
 package com.vipul.rest.messenger.service;
 
+import java.util.List;
+import com.vipul.rest.messenger.model.*;
+
+
 public class MessageService {
 
-	
+	public List<Message> getAllMessages(){
+		
+	}
 	
 }
