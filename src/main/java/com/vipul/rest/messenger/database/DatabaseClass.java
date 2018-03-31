7fbfb4426bfd0cfa385afb6df00f796bb827192b
vipul parmar <vipulparmar154@gmail.com>
2018-03-31 00:03:39 -0700

    Added getMessage method to search method by id

---
+++ src/main/java/com/vipul/rest/messenger/database/DatabaseClass.java
@@ -0,0 +1,24 @@
+package com.vipul.rest.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.vipul.rest.messenger.model.Message;
+import com.vipul.rest.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+	
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+	
+	
+	
+}
--- src/main/java/com/vipul/rest/messenger/service/MessageService.java
+++ src/main/java/com/vipul/rest/messenger/service/MessageService.java
@@ -2,19 +2,53 @@ package com.vipul.rest.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.vipul.rest.messenger.model.*;
+import java.util.Map;
 
+import com.vipul.rest.messenger.database.DatabaseClass;
+import com.vipul.rest.messenger.model.*;
 
 public class MessageService {
 
-	public List<Message> getAllMessages(){
-		Message m1 = new Message(1L, "Hello World", "Vipul Parmar");
-		Message m2 = new Message(2L, "Good evening !!", "John Smith");
-		
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+	public MessageService() {
+		messages.put(1L, new Message(1, "what's up?", "Vipul Parmar"));
+		messages.put(2L, new Message(2, "Hey There !!", "Vipul Parmar"));
 	}
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+
+	public List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values());
+	}
+
+	// getMessage
+
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+
+	// addMessage
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+
+	// updateMessage
+
+	public Message updateMessage(Message message) {
+
+		if (message.getId() <= 0) {
+			return null;
+		}
+
+		messages.put(message.getId(), message);
+		return message;
+	}
+
+	// removeMessage
+
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+
 }
