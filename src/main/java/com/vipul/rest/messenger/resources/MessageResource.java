1c715fa90ac41f1eb09dacc1de570eea0a57fe54
Vipul Parmar <vipulparmar154@gmail.com>
2018-04-11 01:39:21 -0300

    creating ProfileResource

--- src/main/java/com/vipul/rest/messenger/resources/MessageResource.java
+++ src/main/java/com/vipul/rest/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package com.vipul.rest.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,22 +16,40 @@ import com.vipul.rest.messenger.model.Message;
 import com.vipul.rest.messenger.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
-	
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessage() {
 		return messageService.getAllMessages();
 	}
 
+	@POST
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMessage( @PathParam("messageId")long id) {
+	public Message getMessage(@PathParam("messageId") long id) {
 		return messageService.getMessage(id);
-		//return "test";
+		// return "test";
 	}
-	
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+
 }
---
+++ src/main/java/com/vipul/rest/messenger/resources/ProfileResource.java
@@ -0,0 +1,55 @@
+package com.vipul.rest.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.vipul.rest.messenger.model.Profile;
+import com.vipul.rest.messenger.service.ProfileService;
+
+@Path("/profiles")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProfileResource {
+
+	private ProfileService profileService = new ProfileService();
+
+	@GET
+	public List<Profile> getProfiles() {
+		return profileService.getProfiles();
+
+	}
+
+	@POST
+	public Profile addProfile(Profile profile) {
+		return profileService.addProfile(profile);
+	}
+
+	@GET
+	@Path("/{profileName}")
+	public Profile getProfile(@PathParam("profileName") String profileName) {
+		return profileService.getProfile(profileName);
+	}
+
+	@PUT
+	@Path("/{profileName}")
+	public Profile updateProfile(@PathParam("profileName") String profileName, Profile profile) {
+		profile.setProfileName(profileName);
+		return profileService.updateProfile(profile);
+	}
+
+	@DELETE
+	@Path("/{profileName}")
+	public void deleteProfile(@PathParam("profileName") String profileName) {
+		profileService.deleteProfile(profileName);
+	}
+	
+}
